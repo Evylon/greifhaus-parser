fa2034e1637706322e26b99241c7460ab07946f1
Evylon <mail@evylon.de>
2020-07-05 23:14:34 +0200

    add new output for latest data point

--- parser.py
+++ parser.py
@@ -28,23 +28,28 @@ def parseTarget(target, targetUrl):
         Log.log(Log.error, 'Failed to parse: currentVisitors = {}, currentFree = {}'.format(currentVisitors, currentFree))
         exit(-1)
 
-    csvFile = os.path.join(outputDir, '{}-counter.csv'.format(target))
-    csvExists = os.path.exists(csvFile)
+    counterFile = os.path.join(outputDir, '{}-counter.csv'.format(target))
+    latestDataFile = os.path.join(outputDir, '{}-latest.csv'.format(target))
+    csvExists = os.path.exists(counterFile)
     lastEntry = None
+    currentTime = datetime.now().replace(microsecond=0).isoformat()
+    newEntry = '{},{},{}\n'.format(currentTime, currentVisitors, currentFree)
 
     if csvExists:
-        with open(csvFile, 'r') as outputCSV:
+        with open(counterFile, 'r') as outputCSV:
             for line in outputCSV:
                 pass
             lastEntry = line
 
-    with open(csvFile, 'a') as outputCSV:
+    with open(counterFile, 'a') as outputCSV:
         if not csvExists:
             outputCSV.write('time,visitors,available\n')
-        currentTime = datetime.now().replace(microsecond=0).isoformat()
-        newEntry = '{},{},{}\n'.format(currentTime, currentVisitors, currentFree)
         if not lastEntry or not lastEntry.partition(',')[2] == newEntry.partition(',')[2]:
             outputCSV.write(newEntry)
+    
+    with open(latestDataFile, 'w') as latestDataCSV:
+        latestDataCSV.write('time,visitors,available\n')
+        latestDataCSV.write(newEntry)
 
 def getClientCount(url):
     html = urllib.request.urlopen(url).read()
